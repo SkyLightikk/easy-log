dc122701bb41588f6c394a12ce992c138442c76d
GitHub Action <action@github.com>
2020-07-22 15:04:40 +0000

    Template cleanup

--- src/main/kotlin/com/github/skylightikk/easylog/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/skylightikk/easylog/listeners/MyProjectManagerListener.kt
@@ -1,8 +1,8 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.skylightikk.easylog.listeners
 
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.skylightikk.easylog.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/skylightikk/easylog/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/skylightikk/easylog/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.skylightikk.easylog.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.skylightikk.easylog.MyBundle
 
 class MyApplicationService {
 
